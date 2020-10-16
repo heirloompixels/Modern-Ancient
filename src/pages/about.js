4f3f1717eb11462c26dea42a20cc605880a6aed3
Kyle Cunningham <analogkyle@gmail.com>
2020-10-16 15:59:59 -0600

    Update about.js
    Added first draft of the about copy.

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,15 @@ export default function About() {
   return (
     <div style={{ color: `teal` }}>
       <Header />
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
+    
+<h1>Experiments in time and memory. </h1>
+<p>Jeannie and Kyle have been adventuring and creating art together since the spring of 2006.  We&#39;ve hiked, biked, climbed, motorcyled, driven, flown and sailed all over the world together.  </p>
+<p>We live in Truth or Consequencs, NM.  A quaint desert oasis located on the banks of the Rio Grande on top of a deep undergound geo thermal aquaduct.  The world here moves in it&#39;s own ways and time takes on a different meaning.  The ancientness of this place is unescapable - fossils from epochs long forgotten littler the land scape.  Whole petrified palm trees splaid out on the earth.  Amazing.  Signs of ancient people everywhere - from the archaic to this moment when humans are still here creating future aftifacts. </p>
+<p>Our studio and home is named Elephant Nest.  We frequently live nomadically in our current camping vehicle, Nook, throughout the American West though we seldom stray far from the Rocky Mountain Range.  </p>
+<p>This website is on going task into ordering our personal history, a task often refered to as a memex.  It&#39;s a raw database of our own knowledge, experiences and creations from which we are able to mine stories which we collect limited edition books.</p>
+<p>The site is organized based off the principles of the intentionally confusing manifesto which informs much of our life&#39;s ways.  </p>
+
+    
     </div>
   )
 }
