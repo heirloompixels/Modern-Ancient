4d9b3f147b15ca23eaf45552e7dd10bf210f0777
Kyle Cunningham <heirloompixels@users.noreply.github.com>
2020-10-27 21:13:23 -0600

    Added Pugsley and Camping pages.

---
+++ src/pages/camping.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function Camping() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header />
+      <h1>Camping Compendium</h1>
+      <ul>
+        <li>Backpacking</li>
+        <li>Nook</li>
+        <li>Trike</li>
+        <li>Home Made Space Ship </li>
+        <li>Karl Felix</li>
+        <li>Unt</li>
+      </ul>
+      
+      <h2>Backpacking</h2>
+      <h2>Nook</h2>
+      
+    </div>
+  )
+}
---
+++ src/pages/pugsley.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function Pugsley() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header />
+      <h1>The Bike: A Pugsley</h1>
+      <p>Extraterestial explorer is what this dude is.</p>
+      
+    </div>
+  )
+}
